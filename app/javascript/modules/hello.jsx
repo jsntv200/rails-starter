f8b99cb6d981ad087cf042617935edf5622bb4be
Jason Taylor <jason@redant.com.au>
2019-03-28 23:11:14 +1100

    Initial rails webpacker react install

---
+++ app/javascript/modules/hello.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Hello = props => (
+  <div>Hello {props.name}!</div>
+)
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,19 @@
+/* eslint no-console:0 */
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Hello } from '../modules/hello';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
